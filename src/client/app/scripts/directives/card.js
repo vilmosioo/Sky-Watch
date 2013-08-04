65f00f5f8323505d6fbc91eeda514de9b7a6ab1c
Vilmos Ioo <cool.villi@gmail.com>
2013-08-04 05:05:21 +0100

    Mixing server results and ordering by magnitude.
    Client uses new server API and displays planets as well.

--- src/client/app/scripts/directives/card.js
+++ src/client/app/scripts/directives/card.js
@@ -8,6 +8,32 @@ angular.module('ngApp')
       restrict: 'E',
       scope: {
         item : '='
+      },
+      controller : function($scope){
+        if(angular.isArray($scope.item.ephemerids) && $scope.item.ephemerids.length){
+          $scope.item.names = [$scope.item.name];
+          $scope.item.type = 'Planet';
+
+          var RA = 0, DE = 0;
+
+          angular.forEach($scope.item.ephemerids, function(ephemerid){
+            RA += parseInt(ephemerid.RAh, 10) + parseInt(ephemerid.RAm, 10) / 60 + parseFloat(ephemerid.RAs, 10) / 3600;
+            DE += parseInt(ephemerid.DEd, 10) + parseInt(ephemerid.DEm, 10) / 60 + parseFloat(ephemerid.Des, 10) / 3600;
+
+            $scope.item.constelation = ephemerid.constellation;
+          });
+
+          RA /= $scope.item.ephemerids.length;
+          DE /= $scope.item.ephemerids.length;
+
+          $scope.item.RAh = Math.floor(RA);
+          $scope.item.RAm = Math.floor((RA - Math.floor(RA)) * 60);
+          $scope.item.RAs = Math.floor((RA - $scope.item.RAh - $scope.item.RAm / 60) * 3600);
+
+          $scope.item.DEd = Math.floor(DE);
+          $scope.item.DEm = Math.floor((DE - Math.floor(DE)) * 60);
+          $scope.item.DEs = Math.floor((DE - $scope.item.DEd - $scope.item.DEm / 60) * 3600);
+        }
       }
     };
   });
--- src/client/app/scripts/services/sky.js
+++ src/client/app/scripts/services/sky.js
@@ -5,7 +5,7 @@ angular.module('ngApp')
     return {
       getItems : function(params){
         var items = [],
-          ngcUrl = 'http://vilmosioo.co.uk/sky-watch-server/public/index.php/v1/ngc',
+          ngcUrl = 'http://vilmosioo.co.uk/sky-watch-server/public/index.php/v1',
           limit = angular.isNumber(params.limit) ? parseInt(params.limit, 10) : 10,
           offset = angular.isNumber(params.offset) ? parseInt(params.offset, 10) : 0;
 
