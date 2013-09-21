5ba6ea9716c6fec698a4dcd3bfdf77830b942760
Vilmos Ioo <cool.villi@gmail.com>
2013-09-21 21:07:20 +0100

    Added menu directive.

---
+++ src/client/app/scripts/directives/menu.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('ngApp')
+  .directive('menu', function isLoading() {
+    return {
+      templateUrl: '/views/templates/menu.html',
+      replace: true,
+      restrict: 'E',
+      scope: {},
+      controller: function($scope){
+        $scope.menu = {
+          active: 0,
+          items: [
+            {
+              href: '/',
+              label: 'Home'
+            },
+            {
+              href: '/browse',
+              label: 'Browse'
+            }
+          ]
+        };
+      }
+    };
+  });
