2ea46269147634dcdeac11fdcb9062f65fbb5f50
Vilmos Ioo <vilmosi@blinkbox.com>
2014-03-20 16:20:08 +0000

    Automated versioning.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-# run build command (default), assuming we are in the client folder
+echo 'Switching to master and setting identity for git'
+git checkout master
+git config user.name $GIT_NAME
+git config user.email $GIT_EMAIL
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
+
+echo 'Patching version...'
+npm version patch -m "Updating version [skip ci]" || { echo 'Version patch failed' ; exit 1; }
+
+echo 'Running build command (default), assuming we are in the client folder'
 cd src/client || { echo 'Could not find src folder' ; exit 1; }
 grunt || { echo 'Build failed' ; exit 1; }
 
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-# deploy to FTP server, assuming we are in the client folder
+echo 'Pushing git data to repo...'
+git push origin master || { echo 'Git push failed.' ; exit 1; }
+git push origin master --tags || { echo 'Git tagging failed.' ; exit 1; }
+
+echo 'deploy to FTP server, assuming we are in the client folder'
 cd src/client || { echo 'Could not find src folder' ; exit 1; }
 grunt replace || { echo 'Replace failed.' ; exit 1; }
 grunt ftp-deploy || { echo 'Deployment failed.' ; exit 1; }
