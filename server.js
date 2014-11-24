bed9b353182d7534c16071286ca5114b46bab3c1
Vilmos Ioo <vilmosi@blinkbox.com>
2014-11-24 18:01:06 +0000

    Added body parser

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
 #!/bin/env node
 //  OpenShift sample Node application
-var express = require('express');
-
-
-//  Set the environment variables we need.
-var host = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-if (!host){
-    console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-};
+var express = require('express'),
+	bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 
 app.use(function(req, res){
     res.setHeader('Content-Type', 'application/json');
     res.send({message: 'It works!'});    
 });
 
-app.listen(port, host, function(){
-    console.log('Server listening on ' + port);
+var server = app.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080, process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1", function(){
+    console.log('Server listening on ' + server.address().port);
 });
