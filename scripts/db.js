9f554973837b5c36e1349bf4de83d78e0aa5adba
Vilmos Ioo <vilmosi@blinkbox.com>
2014-11-25 10:11:50 +0000

    Set up dev environment.
    Refactored db.js

--- scripts/db.js
+++ scripts/db.js
@@ -1,28 +1,28 @@
 'use strict';
 
-var mysql = require('mysql');
-
-var DB = function(){
-	this.connection = mysql.createConnection({
-		host: process.env.OPENSHIFT_MYSQL_DB_HOST,
-		port: process.env.OPENSHIFT_MYSQL_DB_PORT,
-		user: process.env.OPENSHIFT_MYSQL_DB_USERNAME,
-		password: process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
+var mysql = require('mysql'),
+	connection = mysql.createConnection({
+		host: process.env.OPENSHIFT_MYSQL_DB_HOST || '127.0.0.1',
+		port: process.env.OPENSHIFT_MYSQL_DB_PORT || 8889,
+		user: process.env.OPENSHIFT_MYSQL_DB_USERNAME || 'root',
+		password: process.env.OPENSHIFT_MYSQL_DB_PASSWORD || 'root',
 		database: 'skywatch'
 	});
-	this.connection.connect();
+
+// this is where we connect to the database
+connection.connect();
+
+var NGC = {
+	get: function(id, cb){
+		this.find('id=' + id, cb);
+	},
+	find: function(where, cb){
+		connection.query('SELECT * FROM `ngc` WHERE ' + where, function(err, rows, fields){
+			cb(err, rows, fields);
+		});
+	}
 };
 
-var NGC = function(){};
-NGC.prototype.get = function(id, cb){
-	this.find('id=' + id, cb);
+module.exports = {
+	NGC: NGC
 };
-NGC.prototype.find = function(where, cb){
-	this.connection.query('SELECT * FROM `ngc` WHERE ' + where, function(err, rows, fields){
-		cb(err, rows, fields);
-	});
-};
-
-DB.prototype.NGC = new NGC();
-
-module.exports = new DB();
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-#!/bin/env node
-//  OpenShift sample Node application
+'use strict';
+
 var express = require('express'),
 	bodyParser = require('body-parser'),
+	logger = require('morgan'),
 	routes = require('./routes');
 
 var app = express();
+app.use(logger('combined'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 	
 app.get('/ngc/:id', routes.ngc);
+app.use(function(req, res){
+	res.send(404);
+});
 
-var server = app.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080, process.env.OPENSHIFT_NODEJS_IP, function(){
-	console.log('Server listening on ' + server.address().port);
+var server = app.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080, process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1', function(){
+	console.log('Server listening on ' + server.address().address + ':' + server.address().port);
 });
