75410b79bd7ca6ab1e9a9821811e3ba68a3c948f
Vilmos Ioo <vilmosi@blinkbox.com>
2014-11-25 10:47:49 +0000

    Added headers

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ var app = express();
 app.use(logger('combined'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
-	
+
+app.use(function(req, res, next){
+	res.setHeader('Content-Type', 'application/json');
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	// res.setHeader('Cache-Control', 'max-age='+(6*24*3600)+', public');
+	next();
+});	
 app.get('/ngc/:id?', routes.ngc);
 app.use(function(req, res){
 	res.send(404);
