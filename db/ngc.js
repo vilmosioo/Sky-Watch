686298837887709f3407432f3cc8c8a786d46eec
Vilmos Ioo <vilmosi@blinkbox.com>
2014-11-25 11:39:29 +0000

    Index for ngc

--- db/ngc.js
+++ db/ngc.js
@@ -4,10 +4,26 @@ var connection = require('./collection');
 
 module.exports = {
 	get: function(id, cb){
-		this.find('id=' + id, cb);
+		if(!id){
+			cb({
+				message: 'An id is required'
+			});
+			return;
+		}
+		connection.query('SELECT * FROM `ngc` WHERE id=' + id, function(err, rows, fields){
+			cb(err, rows, fields);
+		});
 	},
-	find: function(where, cb){
-		connection.query('SELECT * FROM `ngc` WHERE ' + where, function(err, rows, fields){
+	all: function(options, cb){
+		// options should be sanitised by now
+		var query = 'SELECT * FROM `ngc`';
+		query += ' ORDER BY ' + options.orderby.map(function(field){
+			return field + ' ' + options.desc;
+		}).join(', ');
+		query += ' LIMIT ' + options.limit;
+		query += ' OFFSET ' + options.offset;
+
+		connection.query(query, function(err, rows, fields){
 			cb(err, rows, fields);
 		});
 	}
--- routes/ngc.js
+++ routes/ngc.js
@@ -1,12 +1,34 @@
 'use strict';
 
-var db = require('../db');
+var NGC = require('../db').NGC,
+	options = {
+		orderby: {
+			'magnitude': ['magnitude'],
+			'RA': ['RAh', 'RAm'],
+			'DE': ['DEh', 'DEm']
+		},
+		desc: ['ASC', 'DESC']
+	};
 
 module.exports = function(req, res){
-	db.NGC.get(req.params.id || 1, function(err, rows){
-		if(err){
-			return res.send(400, err);
-		}
-		res.send(rows);
-	});
+	if(req.params.id){
+		NGC.get(req.params.id, function(err, rows){
+			if(err){
+				return res.send(400, err);
+			}
+			res.send(rows);
+		});	
+	} else {
+		NGC.all({
+			limit: (req.param('limit') && isFinite(req.param('limit')) && req.param('limit') < 50) ? req.param('limit') : 10,
+			offset: req.param('offset') && isFinite(req.param('offset')) ? req.param('offset') : 0,
+			orderby: options.orderby[req.param('orderby')] || options.orderby['magnitude'],
+			desc: (req.param('desc') && options.desc.indexOf(req.param('desc')) !== -1) ? req.param('desc') : options.desc[0]
+		}, function(err, rows){
+			if(err){
+				return res.send(400, err);
+			}
+			res.send(rows);
+		});	
+	}
 };
