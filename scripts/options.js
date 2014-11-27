f5c0f72d1ea2eeaf5ba2e940ea673caebf896f34
Vilmos Ioo <vilmosi@blinkbox.com>
2014-11-27 15:59:57 +0000

    Using desc

--- scripts/options.js
+++ scripts/options.js
@@ -8,13 +8,18 @@ module.exports = function(req, res, next){
 			'RA': ['RAh', 'RAm'],
 			'DE': ['DEd', 'DEm']
 		},
-		desc: ['ASC', 'DESC']
+		desc: {
+			'ASC': 'ASC',
+			'DESC': 'DESC'
+		}
 	};
 
 	req.options = {
 		limit: (req.param('limit') && isFinite(req.param('limit')) && req.param('limit') < 50) ? req.param('limit') : 10,
 		offset: req.param('offset') && isFinite(req.param('offset')) ? req.param('offset') : 0,
-		order: options.orderby[req.param('orderby')] || options.orderby['magnitude']
+		order: (options.orderby[req.param('orderby')] || options.orderby.magnitude).map(function(field){
+			return [field, options.desc[req.param('desc')] || options.desc.ASC]
+		})
 	};
 
 	next();
