46d9a0a2f6284caa86c0a9846a1495225f7d9a5f
Vilmos Ioo <vilmosi@blinkbox.com>
2014-11-28 15:21:43 +0000

    Retrieving ephemerids

--- routes/planet.js
+++ routes/planet.js
@@ -1,9 +1,20 @@
 'use strict';
 
-var models = require('../models');
+var models = require('../models'),
+	julian = require('julian');
 
 module.exports = function(req, res){
-	models.Planet.findAll().then(function(planets){
+	var now = julian(new Date());
+	models.Planet.findAll({
+		include: [{
+			model: models.Ephemerid,
+			where: {
+				JD: {
+					between: [now - 1, now + 1] // retrieving ephemerids for +-1 day
+				}
+			}
+		}]
+	}).then(function(planets){
 		res.send({
 			title: 'Planets',
 			results: planets || []
