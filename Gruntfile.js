3f87fed776a9cfc2f2d710499cf4ec83bcef616c
Vilmos Ioo <vilmosi@blinkbox.com>
2014-12-01 15:22:08 +0000

    Jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt){
 	grunt.initConfig({
 		jshint: {
 			options: {
-				// jshintrc: '.jshintrc'
+				jshintrc: '.jshintrc'
 			},
 			main: ['**/*.js', '!node_modules/**']
 		}
