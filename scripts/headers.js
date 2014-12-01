c3e3471442d2d428e8c49e923a45da49b10794a2
Vilmos Ioo <vilmosi@blinkbox.com>
2014-12-01 17:17:28 +0000

    Added jshint header

--- scripts/headers.js
+++ scripts/headers.js
@@ -4,5 +4,6 @@ module.exports = function(req, res, next){
 	res.setHeader('Content-Type', 'application/json');
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Cache-Control', 'max-age='+(6*24*3600)+', public');
+	res.setHeader('X-JSHint', 'True');
 	next();
 };
