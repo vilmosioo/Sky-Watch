76f27362392490767362038e923a8d7f44b5a533
Vilmos Ioo <vilmosi@blinkbox.com>
2014-12-01 18:09:38 +0000

    Removed hot deploy and changed header

--- scripts/headers.js
+++ scripts/headers.js
@@ -4,6 +4,6 @@ module.exports = function(req, res, next){
 	res.setHeader('Content-Type', 'application/json');
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Cache-Control', 'max-age='+(6*24*3600)+', public');
-	res.setHeader('X-JSHint', 'True');
+	res.setHeader('X-JSHINT', 'True');
 	next();
 };
