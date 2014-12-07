a5dd185193d29daa4a5558b5da54237fdc17239c
Vilmos Ioo <vilmosi@blinkbox.com>
2014-12-07 21:05:43 +0000

    Travis should only deploy artefacts

---
+++ ci/before_deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# prepare artefact for deployment
+
+rm -rf node_modules
+rm -rf client
+rm -rf test
+rm .bowerrc
+rm .jshintrc
+rm bower.json
+rm protractor.conf.js
+rm karma.conf.js
+rm Gruntfile.js
+rm .gitignore
