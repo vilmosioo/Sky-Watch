ef5d1847cd37991308f81932dd2f22a0ee100b8a
Vilmos Ioo <vilmosi@blinkbox.com>
2014-12-07 22:31:01 +0000

    Git rm stuff

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -2,16 +2,16 @@
 
 # prepare artefact for deployment
 
-rm -rf node_modules
-rm -rf client
-rm -rf test
-rm .bowerrc
-rm .jshintrc
-rm bower.json
-rm protractor.conf.js
-rm karma.conf.js
-rm Gruntfile.js
-rm .gitignore
+git rm -rf node_modules
+git rm -rf client
+git rm -rf test
+git rm .bowerrc
+git rm .jshintrc
+git rm bower.json
+git rm protractor.conf.js
+git rm karma.conf.js
+git rm Gruntfile.js
+git rm .gitignore
 
 git add dist
 git commit -m "Artefacts generated"
