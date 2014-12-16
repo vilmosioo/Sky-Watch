b21ec096ff7b205fb824d684da23f710506c25c3
Vilmos Ioo <vilmosi@blinkbox.com>
2014-12-16 13:43:08 +0000

    Adding artefacts on openshift branches

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -1,23 +1,15 @@
 #!/bin/sh
 
-# prepare artefact for deployment
+# prepare artefacts for deployment
 
-git rm -rf client
-git rm -rf test
-git rm .bowerrc
-git rm .jshintrc
-git rm bower.json
-git rm .gitignore
-git rm protractor.conf.js
-git rm karma.conf.js
-git rm Gruntfile.js
+git checkout openshift/skywatch
+git rm -r public
+git add public
+git commit -m "Artefacts generated"
 
-# files ignored by git are not removed
-
-rm -rf client
-rm -rf .sass-cache
-rm -rf coverage
-rm -rf node_modules
-
+git checkout openshift/nginx
+git rm -r dist
 git add dist
 git commit -m "Artefacts generated"
+
+git checkout master
