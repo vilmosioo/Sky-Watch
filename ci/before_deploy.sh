e026eb8966ffd3664dcb1c766644d0c189f9e728
Vilmos Ioo <vilmosi@blinkbox.com>
2014-12-16 15:04:10 +0000

    Updating github as well

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 # prepare artefacts for deployment
-# TODO update github
 
 git checkout openshift/skywatch
 git rm -r public
 git add public
 git commit -m "Artefacts generated"
+git push origin openshift/skywatch
 
 git checkout openshift/nginx
 git rm -r dist
 git add dist
 git commit -m "Artefacts generated"
+git push origin openshift/nginx
 
 git checkout master
