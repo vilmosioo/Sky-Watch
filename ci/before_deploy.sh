8015039fca589b1812394b48135f33eb264765b0
Vilmos Ioo <vilmosi@blinkbox.com>
2014-12-17 10:16:05 +0000

    Deploying only to nginx cartridge

---
+++ ci/before_deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# prepare artefact for deployment
+
+git rm -rf client
+git rm -rf test
+git rm .bowerrc
+git rm .jshintrc
+git rm bower.json
+git rm .gitignore
+git rm protractor.conf.js
+git rm karma.conf.js
+git rm Gruntfile.js
+
+# files ignored by git are not removed
+
+rm -rf client
+rm -rf .sass-cache
+rm -rf .tmp
+rm -rf coverage
+rm -rf node_modules
+
+git add dist
+git add public
+git commit -m "Artefacts generated"
