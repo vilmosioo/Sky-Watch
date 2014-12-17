f124e40f9a441858c6b262bb2c1b07c4a6b8d671
Vilmos Ioo <vilmosi@blinkbox.com>
2014-12-17 10:37:28 +0000

    Express using port 15000

--- server.js
+++ server.js
@@ -39,7 +39,9 @@ app.use('/api', router);
 
 console.log('Bootstrapping application, this may take a moment...');
 models.bootstrap().then(function(){
-	var server = app.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080, process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1', function(){
+	// https://help.openshift.com/hc/en-us/articles/202185874
+	// ports 15000 - 35530 are available on openshift
+	var server = app.listen(15000, process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1', function(){
 		console.log('Server listening on ' + server.address().address + ':' + server.address().port);
 	});
 }, function(err){
