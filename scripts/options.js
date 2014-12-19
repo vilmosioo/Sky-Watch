f63ed3beeb788ab8d5b1373dd4d32f532e6aebdc
Vilmos Ioo <vilmosi@blinkbox.com>
2014-12-19 13:56:21 +0000

    Increased limit to 50

--- scripts/options.js
+++ scripts/options.js
@@ -14,7 +14,7 @@ var options = {
 
 module.exports = function(req, res, next){
 	req.options = {
-		limit: (req.param('limit') && isFinite(req.param('limit')) && req.param('limit') < 50) ? req.param('limit') : 10,
+		limit: (req.param('limit') && isFinite(req.param('limit')) && req.param('limit') <= 50) ? req.param('limit') : 10,
 		offset: req.param('offset') && isFinite(req.param('offset')) ? req.param('offset') : 0,
 		order: (options.orderby[req.param('orderby')] || options.orderby.magnitude).map(function(field){
 			return [field, options.desc[req.param('desc')] || options.desc.ASC];
