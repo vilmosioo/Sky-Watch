637ef934017953ccb32acfca61417655b12118ca
Vilmos Ioo <cool.villi@gmail.com>
2016-03-05 16:33:42 +0000

    Regenerated tokens

--- ci/build.sh
+++ ci/build.sh
@@ -2,8 +2,8 @@
 
 echo 'Switching to master and setting identity for git'
 git checkout master
-git config user.name $GIT_NAME
-git config user.email $GIT_EMAIL
+git config user.name $GITHUB_USER
+git config user.email $GITHUB_EMAIL
 git config credential.helper "store --file=.git/credentials"
 git config remote.origin.url https://github.com/vilmosioo/Sky-Watch.git
 echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
