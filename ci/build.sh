a8ea9d845f415c9972c07b674848cd96b9a5a369
Vilmos Ioo <cool.villi@gmail.com>
2016-03-12 16:24:12 +0000

    Not patching version on PR

--- ci/build.sh
+++ ci/build.sh
@@ -8,10 +8,9 @@ if [[ $TRAVIS_BRANCH == "master" ]] && [[ $TRAVIS_PULL_REQUEST == false ]]; then
 	git config credential.helper "store --file=.git/credentials"
 	git config remote.origin.url https://github.com/vilmosioo/Sky-Watch.git
 	echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
+	echo "Patching version..."
+	npm version patch -m "Updating version [skip ci]" || { echo "Version patch failed" ; exit 1; }
 fi
-
-echo "Patching version..."
-npm version patch -m "Updating version [skip ci]" || { echo "Version patch failed" ; exit 1; }
 
 echo "Running build command (default), assuming we are in the client folder"
 grunt || { echo "Client build failed" ; exit 1; }
