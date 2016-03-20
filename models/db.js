5a613c1e611139b40131a4487db25eb5acb80e1e
Vilmos Ioo <cool.villi@gmail.com>
2016-03-20 18:16:14 +0000

    Updated location

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@
 
 var Sequelize = require('sequelize');
 
-module.exports = new Sequelize('skywatch', process.env.OPENSHIFT_MYSQL_DB_USERNAME || 'root', process.env.OPENSHIFT_MYSQL_DB_PASSWORD || 'root', {
+module.exports = new Sequelize('skywatch', process.env.OPENSHIFT_MYSQL_DB_USERNAME || 'root', process.env.OPENSHIFT_MYSQL_DB_PASSWORD || '', {
 	host: process.env.OPENSHIFT_MYSQL_DB_HOST || '127.0.0.1',
 	port: process.env.OPENSHIFT_MYSQL_DB_PORT || 8889,
 	define: {
