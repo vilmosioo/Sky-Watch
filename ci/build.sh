0db06552ddfbed646dc60f1e2a5ab683c8e07f52
Vilmos Ioo <cool.villi@gmail.com>
2016-03-20 16:51:18 +0000

    Test

--- ci/build.sh
+++ ci/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sh -h
+
 if [[ $TRAVIS_BRANCH == "master" ]] && [[ $TRAVIS_PULL_REQUEST == false ]]; then
 	echo "Switching to master and setting identity for git"
 	git checkout master
